1ff504a2cec7f12fdc10ab18fe5b06395fdc03bc
nasa14119 <nico.amaya0205@gmail.com>
2026-01-29 16:52:35 -0600

    feat(oasis): first mock of oasis

---
+++ src/i18n/es/oasis.ts
@@ -0,0 +1,9 @@
+import type { OasisContent } from "../layouts/oasis";
+export default {
+  main_sections: {
+    title: "oasis monarca",
+    main_entrance: [
+      "La mejor manera en la que podemos ayudar a las mariposas en la ciudad. Donde se une el diseño, estetica y la ayuda al planeta",
+    ],
+  },
+} satisfies OasisContent;
---
+++ src/i18n/layouts/oasis.ts
@@ -0,0 +1,6 @@
+export interface OasisContent {
+  main_sections: {
+    title: string;
+    main_entrance: string[];
+  };
+}
