835862e9ba3aaea35af892cc92fc6dc6aeab51eb
nasa14119 <nico.amaya0205@gmail.com>
2026-01-29 17:28:58 -0600

    feat(MainSectio): adding the hero section for gardens

---
+++ src/i18n/es/jardines.ts
@@ -0,0 +1,9 @@
+import type { JardinesContent } from "../layouts/jardines";
+export default {
+  main_section: {
+    title: "jardines monarca",
+    main_entrance: [
+      "Un gran forma en la que todos nos beneficiamos. Polinizacion, autoconsumo e ingresos extra",
+    ],
+  },
+} satisfies JardinesContent;
---
+++ src/i18n/layouts/jardines.ts
@@ -0,0 +1,6 @@
+export interface JardinesContent {
+  main_section: {
+    title: string;
+    main_entrance: string[];
+  };
+}
