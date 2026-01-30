25c5e6ef249fbdcb4bd262f48fba144744e4d771
nasa14119 <nico.amaya0205@gmail.com>
2026-01-30 11:58:32 -0600

    feat(MainSection): adding the landing for gardens

--- src/i18n/es/jardines.ts
+++ src/i18n/es/jardines.ts
@@ -3,7 +3,8 @@ export default {
   main_section: {
     title: "jardines monarca",
     main_entrance: [
-      "Un gran forma en la que todos nos beneficiamos. Polinizacion, autoconsumo e ingresos extra",
+      "Una de las mejores maneras en la que puedes ayuddar a mejorar la vida de miles de mexicanos as su vez de salvar el planeta ",
     ],
+    call_to_action: "Contactanos",
   },
 } satisfies JardinesContent;
--- src/i18n/layouts/jardines.ts
+++ src/i18n/layouts/jardines.ts
@@ -2,5 +2,6 @@ export interface JardinesContent {
   main_section: {
     title: string;
     main_entrance: string[];
+    call_to_action: string;
   };
 }
