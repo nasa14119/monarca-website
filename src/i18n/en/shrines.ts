341f922fedaf4a553f2aa481957c1b4bae8741c4
nasa14119 <nico.amaya0205@gmail.com>
2026-02-03 16:14:42 -0600

    feat(shrines): adding shrines page

---
+++ src/i18n/en/shrines.ts
@@ -0,0 +1,24 @@
+import type { Shrines } from "@i18n/layouts/shrines";
+
+export default {
+  header: {
+    title: "Shrines",
+    subtitle: "Where it began",
+    description: [
+      "Our story began when we realized there was no real government support to protect something as vital as monarch butterflies. Their habitats were being deforested, and local communities lacked information about their importance and how they could help.",
+      "Today, we can say that we have solved many of these issues, and we invite you to discover these beautiful butterflies.",
+    ],
+  },
+  main: {
+    visit: "Visit us",
+    descriptors: {
+      location: "Location",
+      web: "Website",
+    },
+  },
+  more: {
+    titile: "Even more",
+    description:
+      "We manage a series of sanctuaries closed to the public, where we focus on conserving nature.",
+  },
+} satisfies Shrines;
